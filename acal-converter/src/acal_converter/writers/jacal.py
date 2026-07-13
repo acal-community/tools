3907f15cf7e22fc219356dc59d7cd1fa45b6aea5
William <32223787+humantypo@users.noreply.github.com>
2026-07-13 11:10:59 -0700

    Revert "acal-converter"

--- acal-converter/src/acal_converter/writers/jacal.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-
-def dump(data: dict, stream=None) -> None:
-    if stream is None:
-        stream = sys.stdout
-    stream.write(json.dumps(data, indent=2))
-    stream.write("\n")
